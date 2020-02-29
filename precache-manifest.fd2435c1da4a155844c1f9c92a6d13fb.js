46708d63353d645e0a39929bacafc36ea3543178
Eric Stein <ericthestein@gmail.com>
2020-02-29 12:43:56 -0500

    Updates

---
+++ precache-manifest.fd2435c1da4a155844c1f9c92a6d13fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b3a63817340d1a7243dabe2997f895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afca47e6f3b11fed1ebf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "37fb1a6760dfc7f3055a",
+    "url": "/static/js/2.2fe34a2f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2fe34a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afca47e6f3b11fed1ebf",
+    "url": "/static/js/main.ac27825a.chunk.js"
+  },
+  {
+    "revision": "3fd63ef97478295b4580",
+    "url": "/static/js/runtime-main.d494ff65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
